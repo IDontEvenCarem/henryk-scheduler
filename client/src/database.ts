2725a4827146214117d299e2e493b15e531d9b35
Kajetan Owczarek <flash3ch@gmail.com>
2022-03-07 22:07:25 +0100

    Database definitons

--- client/src/database.ts
+++ client/src/database.ts
@@ -7,13 +7,46 @@ export interface Todo {
     done: boolean
 }
 
+export interface Event {
+    id?: number,
+    name: string,
+    time_start: Date,
+    time_end: Date,
+    room: string,
+    teacher: string,
+    color: string
+}
+
+export interface RepeatingEvent {
+    id?: number,
+    name: string,
+    room: string,
+    teacher: string,
+    color: string,
+    weekday: string,
+    time_start: string,
+    time_end: string
+}
+
+export interface Note {
+    id?: number,
+    title: string,
+    content: string
+}
+
 export class TypedDexie extends Dexie {
     todos!: Table<Todo>;
+    timetable!: Table<Event>;
+    timetable_repeating!: Table<RepeatingEvent>;
+    notes!: Table<Note>;
 
     constructor() {
         super('testdexie')
-        this.version(1).stores({
-            todos: '++id, done'
+        this.version(2).stores({
+            todos: '++id',
+            timetable: '++id, time_start, time_end',
+            timetable_repeating: '++id, weekday',
+            notes: '++id, title'
         })
     }
 }
