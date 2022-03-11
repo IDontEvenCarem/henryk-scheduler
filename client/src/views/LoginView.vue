35b195a3b61872ee8a64545945dcd75580f5566b
BismarckVonRed <100443321+BismarckVonRed@users.noreply.github.com>
2022-03-11 22:58:37 +0100

    Login page added

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref } from 'vue'
+
+const login = ref('')
+const password = ref('')
+
+function OnLogin()
+  {
+    alert(login.value + ' ' + password.value)
+  }
+</script>
+
+<template>
+  <div class="wrapper">
+    <button v-on:click="$router.go(-1)">
+      Return
+    </button>
+    
+    <div class="center"> 
+      <p>Login</p>
+      <input v-model="login">
+      <p>Password</p>
+
+      <input type="password" v-model="password">
+      <br>
+
+      <button v-on:click="OnLogin">
+        Log in
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .center{
+    display:grid;
+    grid-template-columns:33% 33% 33%;
+  }
+  .center > * {
+    grid-column:2/3;
+  }
+  .wrapper{
+    /*background-color:lightgray;*/
+    /*background: linear-gradient(0.15turn, #e66465, #9198e5);*/ /*We can use it*/
+    background: linear-gradient(0.15turn, #2762c2, #55b350);
+    min-height:100vh;
+  }
+</style>
