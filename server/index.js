c50a8bbc090ddb028f00ae4f32967ef7e51b2d87
wojtequ <wojtequ@gmail.com>
2022-03-12 15:06:21 +0100

    Login system implementation

--- server/index.js
+++ server/index.js
@@ -1,23 +1,74 @@
 const express = require('express')
-const app = express()
-const port = 2999
+const jose = require('jose')
+const argon = require('argon2')
+const e = require('express')
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+async function main() {
+    const app = express()
+    const port = 2999
+    app.use(express.urlencoded({ extended: false }))
 
-app.get('/test', (req, res) => {
-    res.send(req.query['a'] || 'none')
-})
+    const { publicKey, privateKey } = await jose.generateKeyPair('PS256')
+    console.log(publicKey)
+    console.log(privateKey)
 
-app.get('/test2', (req, res) => {
-    if (Math.random() > 0.3)
-        res.send("wiecej")
-    else
-        res.send("mniej")
-})
+    /**  @type {Map<string, number>} */
+    const Database = new Map()
+
+    Database.set('Henryk', await argon.hash('password123'))
+    Database.set('costam', await argon.hash('admin'))
+
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+    })
+
+    app.get('/jwtpublickey', (req, res) => {
+        res.send(publicKey.export({ format: 'jwk' }))
+    })
+
+    app.post('/login', async (req, res) => {
+        try {
+            const { username, password } = req.body
+            if (!(username && password))
+                res.status(400).send("All input is required")
+            if (Database.has(username)) {
+                if (await argon.verify(Database.get(username), password)) {
+                    const jwt = await new jose.SignJWT({ username })
+                        .setProtectedHeader({ alg: 'PS256' })
+                        .setIssuedAt()
+                        .setExpirationTime('2h')
+                        .sign(privateKey)
+
+                    console.log(jwt)
+                    res.send(jwt)
+                }
+                else
+                    res.status(400).send('not ok')
+            }
+            else {
+                res.status(400).send('not ok')
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    })
+    app.post('/register', async (req, res) => {
+        try {
+            if (!(req.body.username && req.body.password))
+                res.status(400).send("All input is required")
+            const hashedPassword = await argon.hash(req.body.password)
+            Database.set(req.body.username, hashedPassword)
+            res.send('user created')
+        } catch (err) {
+            res.status(500).send('fail')
+        }
+        console.log(Database.get(req.body.username))
+    })
 
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
+}
+
+main()
