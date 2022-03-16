9e0365987081e8f22dce29a888d58834d1e449f9
Kajetan Owczarek <flash3ch@gmail.com>
2022-03-16 02:16:22 +0100

    Replace an in-memory map with mongodb

--- server/index.js
+++ server/index.js
@@ -2,8 +2,30 @@ const express = require('express')
 const jose = require('jose')
 const argon = require('argon2')
 const multer = require('multer')
+const mongodb = require('mongodb')
+
 
 async function main() {
+    const client = new mongodb.MongoClient("mongodb://127.0.0.1:27017")
+    await client.connect();
+    const db = await client.db("henryk_scheduler")
+    const users_col = db.collection('users')
+
+    async function save_user (username, password) {
+        if (await users_col.countDocuments({username}) != 0) {
+            throw Error("User already registered with that name")
+        }
+
+        return users_col.insertOne({
+            username,
+            password: await argon.hash(password)
+        })
+    }
+
+    function password_of (username) {
+        return users_col.findOne({username}).then(value => value && value.password)
+    }
+
     const app = express()
     const upload = multer()
     const port = 2999
@@ -12,21 +34,12 @@ async function main() {
     app.use(upload.none())
 
     const { publicKey, privateKey } = await jose.generateKeyPair('PS256')
-    console.log(publicKey)
-    console.log(privateKey)
-
-    /**  @type {Map<string, number>} */
-    const Database = new Map()
-
-    Database.set('Henryk', await argon.hash('password123'))
-    Database.set('costam', await argon.hash('admin'))
 
     app.get('/', (req, res) => {
         res.send('Hello World!')
     })
 
     app.get('/jwtpublickey', async (req, res) => {
-        // res.send(await jose.exportJWK(publicKey))
         res.send(await jose.exportSPKI(publicKey))
     })
 
@@ -35,39 +48,43 @@ async function main() {
             const { username, password } = req.body
             if (!(username && password)) {
                 res.status(400).send("All input is required")
-            } 
-            else if (Database.has(username)) {
-                if (await argon.verify(Database.get(username), password)) {
-                    const jwt = await new jose.SignJWT({ username })
-                        .setProtectedHeader({ alg: 'PS256' })
-                        .setIssuedAt()
-                        .setExpirationTime('2h')
-                        .sign(privateKey)
+            }
+            const passhash = await password_of(username)
+            if (!passhash) {
+                res.status(400).send("wrong credentials")
+                return;
+            }
 
-                    console.log(jwt)
-                    res.send(jwt)
-                }
-                else
-                    res.status(400).send('not ok')
+            if (await argon.verify(passhash, password)) {
+                const jwt = await new jose.SignJWT({ username })
+                    .setProtectedHeader({ alg: 'PS256' })
+                    .setIssuedAt()
+                    .setExpirationTime('2h')
+                    .sign(privateKey)
+
+                console.log(jwt)
+                res.send(jwt)
             }
             else {
                 res.status(400).send('not ok')
             }
         } catch (err) {
+            res.status(500).send("error")
             console.log(err)
         }
     })
     app.post('/register', async (req, res) => {
         try {
-            if (!(req.body.username && req.body.password))
+            if (!(req.body.username && req.body.password)) {
                 res.status(400).send("All input is required")
-            const hashedPassword = await argon.hash(req.body.password)
-            Database.set(req.body.username, hashedPassword)
-            res.send('user created')
+                return
+            }
+            await save_user(req.body.username, req.body.password);
+            res.status(200).send('user created')
         } catch (err) {
+            console.log(err)
             res.status(500).send('fail')
         }
-        console.log(Database.get(req.body.username))
     })
 
 
